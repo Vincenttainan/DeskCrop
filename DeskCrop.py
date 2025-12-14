2f5e3dde132aa527754dd58986a9fa66a5285753
Vincenttainan <54768760+Vincenttainan@users.noreply.github.com>
2025-12-14 22:53:26 +0800

    Add initial DeskCrop.py with GUI setup

---
+++ DeskCrop.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+import Buttons
+
+root = tk.Tk()
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+window_width = screen_width
+window_height = 150
+x = (screen_width - window_width) // 2
+y = (screen_height - window_height) - 30
+
+root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+root.title("DeskCrop")
+root.update_idletasks()
+root.overrideredirect(True)
+root.attributes("-topmost", True)
+
+Buttons.MacCloseButton(
+    parent=root,
+    x=10,
+    y=10,
+    command=root.destroy
+)
+
+root.mainloop()
